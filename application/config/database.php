287289f62062a418b218acfd1d5556dc76b0c2d7
Will Parker <wprk14@gmail.com>
2013-10-13 03:14:41 +0100

    ADD: Installed Flexi_auth library system to start building login system

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'willpk_main';
+$db['default']['password'] = 'Y678wan';
+$db['default']['database'] = 'willpk_manpoints';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
